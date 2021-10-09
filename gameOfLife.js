aaf0191f588720d621b8bc49211412c2c189ded8
Dani Sanchez <danysq87@gmail.com>
2021-10-09 17:36:23 +0200

    Added creadorTablero

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,25 +1,52 @@
 const dimensionesTablero = 5;
- 
- function game (tablero, dimensionesTablero){
-   const creadorTablero = Array(dimensionesTablero)
+const tableroOriginal = Array(dimensionesTablero)
+  .fill(0)
+  .map(() => Array(dimensionesTablero).fill(0));
+
+function juego(tablero) {
+  const creadorTablero = Array(dimensionesTablero)
     .fill(0)
-    .map(()=> Array(dimensionesTablero).fill(0));
- 
-   for (let x = 0; x < tablero.length; x++) {
+    .map(() => Array(dimensionesTablero).fill(0));
+
+  for (let x = 0; x < tablero.length; x++) {
     for (let y = 0; y < tablero[x].length; y++) {
       let contador = 0;
-        if(tablero[x - 1] !== undefined){
-          if(tablero[x - 1][j] === 1)contador++;
-          if(tablero[x - 1][j - 1] === 1)contador++
-          if(tablero[x - 1][j + 1] === 1)contador++
-        }
-        if(tablero[x][j - 1] === 1)contador++;
-        if(tablero[x][j + 1] === 1)contador++;
-        if(tablero[x + 1] !== undefined){
-          if(tablero[x + 1][j] === 1)contador++;
-          if(tablero[x + 1][j - 1] === 1)contador++;
-          if(tablero[x + 1][j + 1] === 1)contador++;
-        }
-        creadorTablero[x][j] = paintCell(tablero[x][j], contador)
+      if (tablero[x + 1] !== undefined) {
+        if (tablero[x + 1][y] === 1) contador++;
+        if (tablero[x + 1][y - 1] === 1) contador++;
+        if (tablero[x + 1][y + 1] === 1) contador++;
       }
+      if (tablero[x][y - 1] === 1) contador++;
+      if (tablero[x][y + 1] === 1) contador++;
+      if (tablero[x - 1] !== undefined) {
+        if (tablero[x - 1][y] === 1) contador++;
+        if (tablero[x - 1][y - 1] === 1) contador++;
+        if (tablero[x - 1][y + 1] === 1) contador++;
+      }
+
+      creadorTablero[x][y] = creandoTablero(tablero[x][y], contador);
     }
+  }
+  juego(creadorTablero);
+}
+function creandoTablero(vivos, vecinos) {
+  if (vivos === 1) {
+    if (vecinos < 2) {
+      return 0;
+    }
+    if (vecinos > 3) {
+      return 0;
+    }
+    if (vecinos === 2 || vecinos === 3) {
+      return 1;
+    }
+  }
+  if (vivos === 0) {
+    if (vecinos === 3) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+console.log(juego(tableroOriginal));
