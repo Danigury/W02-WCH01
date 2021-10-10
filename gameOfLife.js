e7b8144db776aee8a2647c78e727f85855373b0e
Dani Sanchez <danysq87@gmail.com>
2021-10-10 21:30:53 +0200

    Added function to change color of celda

--- gameOfLife.js
+++ gameOfLife.js
@@ -2,8 +2,9 @@ const botonGenerarTablero = document.querySelector(".boton");
 botonGenerarTablero.addEventListener("click", function () {
   this.style.visibility = "hidden";
 });
+const casilla = document.querySelector("td");
 
-const dimensionesTablero = 5;
+const dimensionesTablero = 20;
 const tableroOriginal = Array(dimensionesTablero)
   .fill(0)
   .map(() => Array(dimensionesTablero).fill(0));
@@ -16,9 +17,9 @@ function juego(tablero, count) {
     .fill(0)
     .map(() => Array(dimensionesTablero).fill(0));
 
-  if (count === 0) {
+  /* if (count === 0) {
     return;
-  }
+  } */
 
   for (let x = 0; x < tablero.length; x++) {
     for (let y = 0; y < tablero[x].length; y++) {
@@ -37,12 +38,20 @@ function juego(tablero, count) {
       }
 
       tableroNuevo[x][y] = creandoTablero(tablero[x][y], contador);
+
+      if (tableroNuevo[x][y] === 0) {
+        document.querySelector("td").style.background = "#ccc9bef5";
+      }
+      if (tableroNuevo[x][y] === 1) {
+        document.querySelector("td").style.background = "darkgoldenrod";
+      }
     }
   }
-  console.log(tableroNuevo);
-  juego(tableroNuevo, count - 1);
-  // return creadorTablero;
+  // console.log(tableroNuevo);
+  // juego(tableroNuevo, count - 1);
+  return tableroNuevo;
 }
+
 function creandoTablero(vivos, vecinos) {
   if (vivos === 1) {
     if (vecinos < 2) {
@@ -73,17 +82,21 @@ function creadorTableroHtml() {
   const tabla = document.createElement("table");
   const tblBody = document.createElement("tbody");
 
-  for (let i = 0; i < 20; i++) {
+  for (let x = 0; x < 20; x++) {
     const hilera = document.createElement("tr");
+    tblBody.appendChild(hilera);
+    hilera.classList.add("board__hilera");
 
-    for (let j = 0; j < 20; j++) {
+    for (let y = 0; y < 20; y++) {
       const celda = document.createElement("td");
       const textoCelda = document.createTextNode("");
-
-      celda.appendChild(textoCelda);
       hilera.appendChild(celda);
+      celda.appendChild(textoCelda);
+      celda.className = "board__celda";
+      celda.style.background = "#fdfeee";
+      celda.id = `${x}-${y}`;
+      celda.onclick = cambiandoColorCelda();
     }
-    tblBody.appendChild(hilera);
   }
 
   tabla.appendChild(tblBody);
